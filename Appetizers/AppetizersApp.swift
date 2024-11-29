a5f6ffc1fe5ef9e64e5963220d47abb11132b555
Ravi Ranjan  Ojha <raviranjanojha999@gmail.com>
2024-11-29 23:11:43 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ravi Ranjan  Ojha on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
